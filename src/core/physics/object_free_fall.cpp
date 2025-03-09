e9024db249326d9b615d392199c86de7105ee071
xyzpw <76017734+xyzpw@users.noreply.github.com>
2025-03-09 05:35:29 -0400

    refactor: switch to epoch for airtime timing

--- src/core/physics/object_free_fall.cpp
+++ src/core/physics/object_free_fall.cpp
@@ -2,19 +2,24 @@
 #include "utils/sleep_utils.h"
 #include "utils/object_utils.h"
 #include "utils/physics_utils.h"
+#include "utils/epoch_utils.h"
 
 void handleObjectFreeFall(Object& object)
 {
-    double airtime = 0.0;
-
     double& height = object.height;
     double& velocity = object.velocity;
     double& objectMass = object.mass;
 
+    const double epochAtStart = getEpochAsDecimal();
     const double initHeight = height;
     const double termVelocity = object.terminalVelocity;
     while (height > 0.0)
     {
+        if (!object.isActionUsed) {
+            break;
+        }
+
+        double airtime = getEpochAsDecimal() - epochAtStart;
         double distanceTraveled = calculateFreeFallDistanceAtTime(object, airtime);
         double currentVelocity = calculateFreeFallVelocityAtTime(object, airtime);
 
@@ -28,7 +33,6 @@ void handleObjectFreeFall(Object& object)
         object.position.row = rowAtHeight;
 
         sleepCurrentThread(20);
-        airtime += 0.02;
     }
     velocity = 0.0;
 }
