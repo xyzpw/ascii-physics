135175201571e4a451bc0c135246a8a9e8354db2
xyzpw <76017734+xyzpw@users.noreply.github.com>
2025-03-14 15:14:22 -0400

    refactor: implement drag coefficient variable

--- src/structs/object.cpp
+++ src/structs/object.cpp
@@ -4,11 +4,17 @@
 #include "utils/object_utils.h"
 #include "utils/physics_utils.h"
 #include "core/physics/object_free_fall.h"
+#include "constants/physics_constants.h"
 
 Object::Object(OBJECT_TYPE type, double kg)
 {
     this->type = type;
     this->mass = kg;
+
+    if (type == OBJECT_TYPE::OBJECT_BALL){
+        this->dragCoefficient = DRAG_COEFFICIENT_BALL;
+    }
+
     this->terminalVelocity = calculateTerminalVelocity(*this);
 }
 
