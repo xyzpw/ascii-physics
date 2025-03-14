5280cc180a88731b517432f94186872214cbfbca
xyzpw <76017734+xyzpw@users.noreply.github.com>
2025-03-14 15:10:34 -0400

    refactor: add function to change gravity acceleration value

--- include/constants/physics_constants.h
+++ include/constants/physics_constants.h
@@ -4,3 +4,7 @@ inline double GRAVITY_ACCELERATION = 9.80665;
 inline const double AIR_DENSITY = 1.225;
 
 inline const double DRAG_COEFFICIENT_BALL = 0.47;
+
+inline void changeGravityAcceleration(double v){
+    GRAVITY_ACCELERATION = v;
+}
