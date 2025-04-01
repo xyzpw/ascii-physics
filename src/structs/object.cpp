0c0c571db37a61951de1a74a615d78f76501ab9d
xyzpw <76017734+xyzpw@users.noreply.github.com>
2025-04-01 04:45:07 -0400

    refactor: add function to make resetting object easier

--- src/structs/object.cpp
+++ src/structs/object.cpp
@@ -5,14 +5,16 @@
 #include "core/physics/object_free_fall.h"
 #include "constants/physics_constants.h"
 
+const double PI = 3.141592653589793238;
+
+void configureObjectProperties(Object&);
+
 Object::Object(OBJECT_TYPE type, double kg)
 {
     this->type = type;
     this->mass = kg;
 
-    if (type == OBJECT_TYPE::OBJECT_BALL){
-        this->dragCoefficient = DRAG_COEFFICIENT_BALL;
-    }
+    configureObjectProperties(*this);
 
     this->terminalVelocity = calculateTerminalVelocity(*this);
     this->vectors.acceleration = Vector2D{0.0, -GRAVITY_ACCELERATION};
@@ -26,10 +28,24 @@ void Object::reset()
     this->vectors.acceleration = Vector2D{0.0, -GRAVITY_ACCELERATION};
     this->isActionUsed = false;
     this->terminalVelocity = calculateTerminalVelocity(*this);
+    configureObjectProperties(*this);
 }
 
 void Object::drop()
 {
     std::thread fallThread(handleObjectFreeFall, std::ref(*this));
     fallThread.detach();
+}
+
+void configureObjectProperties(Object& object)
+{
+    switch (object.type)
+    {
+        case OBJECT_TYPE::OBJECT_BALL:{
+            double diameter = 1.0;
+
+            object.dragCoefficient = DRAG_COEFFICIENT_BALL;
+            object.crossSectionalArea = (diameter/2.0) * (diameter/2.0) * PI;
+        }
+    }
 }
