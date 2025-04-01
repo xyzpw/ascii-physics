2c641d3cace48a9dcc329e8123704510aaa81e5c
xyzpw <76017734+xyzpw@users.noreply.github.com>
2025-04-01 05:01:29 -0400

    wip: add drag force variable value

--- src/core/physics/object_physics.cpp
+++ src/core/physics/object_physics.cpp
@@ -12,6 +12,10 @@ void updateObjectPhysicsYAxis(Object& object, double t)
     Vector2D& velocity = object.vectors.velocity;
     Vector2D& position = object.vectors.position;
 
+    double dragForce = calculateDragForce(
+        velocity.y, object.dragCoefficient, object.crossSectionalArea
+    );
+
     const double& acceleration = object.vectors.acceleration.y;
 
     const double yScale = velocity.y >= 0.0 ? 1.0 : -1.0;
