36844eba5b3a83cc098ee76d2969e0a6985a63cf
xyzpw <76017734+xyzpw@users.noreply.github.com>
2025-04-03 09:58:19 -0400

    refactor: add floor collision check for y axis

--- src/core/physics/object_physics.cpp
+++ src/core/physics/object_physics.cpp
@@ -1,5 +1,6 @@
 #include <cmath>
 #include "core/physics/object_physics.h"
+#include "core/physics/object_collision.h"
 #include "structs/object.h"
 #include "utils/physics_utils.h"
 
@@ -9,6 +10,16 @@ void updateObjectPhysicsYAxis(Object& object, double t)
 {
     Vector2D& velocity = object.vectors.velocity;
     Vector2D& position = object.vectors.position;
+
+    bool hasCollidedFloor = checkHasCollidedWithFloor(object);
+    if (hasCollidedFloor){
+        position.y = 0.0;
+        velocity.y *= -object.coefficientOfRestitution;
+        if (std::abs(velocity.y) < VELOCITY_Y_MIN){
+            velocity.y = 0.0;
+            return;
+        }
+    }
 
     double dragForce = calculateDragForce(
         velocity.y, object.dragCoefficient, object.crossSectionalArea
@@ -23,6 +34,14 @@ void updateObjectPhysicsYAxis(Object& object, double t)
     double newVelocity = calculateVelocityFinal(velocity.y, netAcceleration, t);
 
     double newPos = calculateDisplacement(newVelocity, netAcceleration, t);
+
+    // Set velocity and position to 0 if object can't bounce.
+    if (hasCollidedFloor && newPos <= 0.0){
+        velocity.y = 0.0;
+        position.y = 0.0;
+        return;
+    }
+
     newPos += position.y;
 
     velocity.y = newVelocity;
