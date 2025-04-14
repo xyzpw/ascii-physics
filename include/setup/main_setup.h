a37cdda2421ba699952837b81a42d3f0894ff89a
xyzpw <76017734+xyzpw@users.noreply.github.com>
2025-04-14 12:15:48 -0400

    wip: add includes for meters per char implementations

--- include/setup/main_setup.h
+++ include/setup/main_setup.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "structs/object.h"
+#include "structs/world.h"
 #include "enums/object_type.h"
 #include "utils/arg_parser.h"
 
--- src/setup/main_setup.cpp
+++ src/setup/main_setup.cpp
@@ -2,6 +2,7 @@
 #include "setup/main_setup.h"
 #include "setup/input_handler.h"
 #include "structs/object.h"
+#include "structs/world.h"
 #include "enums/object_type.h"
 #include "utils/arg_parser.h"
 #include "utils/screen_utils.h"
