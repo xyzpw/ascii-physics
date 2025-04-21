6b5681a9ed5b6af0aba760167c7ffea51308dd29
xyzpw <76017734+xyzpw@users.noreply.github.com>
2025-04-21 19:42:47 -0400

    refactor: switch variable locations for object input info

--- src/core/input/object_control_scale.cpp
+++ src/core/input/object_control_scale.cpp
@@ -11,7 +11,7 @@ void scaleActiveControl(World& world, double valueStep)
 {
     Object& object = world.getObjectById(world.activeObjectId);
 
-    SELECT_PARAMETER selectParam = world.selectParameter;
+    SELECT_PARAMETER selectParam = world.objectInputInfo.selectParameter;
 
     double minValue = 0;
     double maxValue = 2'147'483'648;
@@ -19,11 +19,11 @@ void scaleActiveControl(World& world, double valueStep)
     if (selectParam == SELECT_PARAMETER::LAUNCH_ANGLE){
         minValue = 0;
         maxValue = 360;
-        double& targetValue = object.launchInfo.launchAngleDeg;
+        double& targetValue = world.objectInputInfo.objectLaunchAngle;
         scaleValue(targetValue, valueStep, minValue, maxValue);
     }
     else if (selectParam == SELECT_PARAMETER::LAUNCH_VELOCITY){
-        double& targetValue = object.launchInfo.velocity;
+        double& targetValue = world.objectInputInfo.objectLaunchVelocity;
         scaleValue(targetValue, valueStep, minValue, maxValue);
     }
 }
