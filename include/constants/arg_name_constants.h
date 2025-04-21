862c91d73afd0302985d0404df1161b68e53f06c
xyzpw <76017734+xyzpw@users.noreply.github.com>
2025-04-21 19:02:32 -0400

    add constants for arg names

---
+++ include/constants/arg_name_constants.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <string>
+
+inline const std::string ARG_NAME_COR = "cor";
+inline const std::string ARG_NAME_CHAR_SIZE = "char-size";
+inline const std::string ARG_NAME_LAUNCH_VELOCITY = "lvelocity";
+inline const std::string ARG_NAME_LAUNCH_ANGLE = "langle";
