6700820d7ddea6bfe1d347da61cc6669a4150247
xyzpw <76017734+xyzpw@users.noreply.github.com>
2025-04-25 17:30:41 -0400

    add equal and not equal operators

--- include/structs/position.h
+++ include/structs/position.h
@@ -3,4 +3,11 @@
 struct Position {
     int column;
     int row;
+
+    bool operator==(const Position& other) const {
+        return other.column == this->column && other.row == this->row;
+    }
+    bool operator!=(const Position& other) const {
+        return other.column != this->column || other.row != this->row;
+    }
 };
