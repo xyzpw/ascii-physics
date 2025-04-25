b7bd4fb78c19eb4492919ff177e9ce19124c335f
xyzpw <76017734+xyzpw@users.noreply.github.com>
2025-04-25 06:05:49 -0400

    feat: add color pairs

--- src/setup/ncurses_setup.cpp
+++ src/setup/ncurses_setup.cpp
@@ -1,5 +1,6 @@
 #include <ncurses.h>
 #include "setup/ncurses_setup.h"
+#include "constants/object_char_constants.h"
 
 void initializeNcurses()
 {
@@ -8,5 +9,7 @@ void initializeNcurses()
     curs_set(0);
     noecho();
     keypad(stdscr, true);
+    start_color();
     use_default_colors();
+    init_pair(ITEM_HIGHLIGHT_YELLOW_PAIR_NUM, COLOR_YELLOW, -1);
 }
