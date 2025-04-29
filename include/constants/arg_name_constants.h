31397740e9eaafadd265a0e2aa1841bc2e6f58a9
xyzpw <76017734+xyzpw@users.noreply.github.com>
2025-04-28 20:53:58 -0400

    feat: add help menu vector

--- include/constants/arg_name_constants.h
+++ include/constants/arg_name_constants.h
@@ -1,8 +1,18 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 inline const std::string ARG_NAME_COR = "cor";
 inline const std::string ARG_NAME_CHAR_SIZE = "char-size";
 inline const std::string ARG_NAME_LAUNCH_VELOCITY = "lvelocity";
 inline const std::string ARG_NAME_LAUNCH_ANGLE = "langle";
+
+const std::vector<std::pair<std::string, std::string>> helpMenu = {
+    {"-m <n>", "mass of object (in kilograms)"},
+    {"-g <n>", "acceleration due to gravity (in m/s [def. 9.80665])"},
+    {"--cor <n>", "coefficient of restitution"},
+    {"--char-size <n>", "size of each character on screen (in meters)"},
+    {"--lvelocity <n>", "launch velocity"},
+    {"--langle <n>", "launch angle (in degrees)"},
+};
