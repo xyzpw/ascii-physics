fb56c6ef3e6c555ea67c56b3da900dde78a27c41
xyzpw <76017734+xyzpw@users.noreply.github.com>
2025-04-30 07:14:01 -0400

    feat: add pair number for red fg

--- include/constants/object_char_constants.h
+++ include/constants/object_char_constants.h
@@ -1,6 +1,7 @@
 #pragma once
 
 inline const int PAIR_NUM_YELLOW_FG = 1;
+inline const int PAIR_NUM_RED_FG = 2;
 
 inline const char* OBJECT_BALL_CHAR = "\u25cf";
 inline const char* FLOOR_CHAR = "\u2501";
