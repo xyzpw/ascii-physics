4e39014da731f86db64fc0410856d830580c1650
xyzpw <76017734+xyzpw@users.noreply.github.com>
2025-04-30 08:08:55 -0400

    add object preset files

---
+++ include/constants/object_preset_constants.h
@@ -0,0 +1,6 @@
+#pragma once
+
+// Tennis ball.
+inline double TENNIS_BALL_MASS = 0.057;
+inline double TENNIS_BALL_DIAMETER = 0.066;
+inline double TENNIS_BALL_COR = 0.84;
---
+++ include/enums/object_preset.h
@@ -0,0 +1,5 @@
+#pragma once
+
+enum class OBJECT_PRESET {
+    TENNIS_BALL,
+};
