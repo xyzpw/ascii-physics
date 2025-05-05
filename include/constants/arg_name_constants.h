e4988ef910fdbc396dda98f2c38fd43e70cdac12
xyzpw <76017734+xyzpw@users.noreply.github.com>
2025-05-05 09:14:40 -0400

    feat: add height max stat arg

--- include/constants/arg_name_constants.h
+++ include/constants/arg_name_constants.h
@@ -9,6 +9,7 @@ inline const std::string ARG_NAME_LAUNCH_VELOCITY = "lvelocity";
 inline const std::string ARG_NAME_LAUNCH_ANGLE = "langle";
 inline const std::string ARG_NAME_STAT_COLLISIONS = "stat-collisions";
 inline const std::string ARG_NAME_STAT_MAXSPEED = "stat-maxspeed";
+inline const std::string ARG_NAME_STAT_HMAX = "stat-hmax";
 inline const std::string ARG_NAME_HIT_FLASH = "hit-flash";
 inline const std::string ARG_NAME_PRESET = "preset";
 inline const std::string ARG_NAME_LIST_PRESET = "list-preset";
@@ -23,6 +24,7 @@ const std::vector<std::pair<std::string, std::string>> helpMenu = {
     {"--langle <n>", "launch angle (in degrees)"},
     {"--stat-collisions", "display the number of collisions for the selected object"},
     {"--stat-maxspeed", "display the maximum speed achieved for the selected object"},
+    {"--stat-hmax", "display the maximum height achieved for the selected object"},
     {"--hit-flash", "objects flash red during collisions"},
     {"--preset <item>", "select object preset"},
     {"--list-preset", "list presets"},
