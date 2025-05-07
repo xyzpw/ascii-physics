d2873a8326df13f081ed0ab356eafe3d84ea3805
xyzpw <76017734+xyzpw@users.noreply.github.com>
2025-05-07 02:19:19 -0400

    feat: add obstacle char

--- include/constants/object_char_constants.h
+++ include/constants/object_char_constants.h
@@ -7,6 +7,7 @@ inline const int PAIR_NUM_DARK_GRAY_FG = 4;
 
 inline const char* OBJECT_BALL_CHAR = "\u25cf";
 inline const char* FLOOR_CHAR = "\u2501";
+inline const char* OBSTACLE_CHAR = "\u25A0";
 
 inline const int TRAIL_FRAME_AGE_MAX = 15;
 inline const int TRAIL_FRAME_AGE_OLD = 9;
