6000918b34692a774a894e9cdbfcaf3053bc64f1
xyzpw <76017734+xyzpw@users.noreply.github.com>
2025-05-11 04:58:18 -0400

    feat: add mouse clicking

--- src/setup/ncurses_setup.cpp
+++ src/setup/ncurses_setup.cpp
@@ -11,6 +11,8 @@ void initializeNcurses()
     keypad(stdscr, true);
     start_color();
     use_default_colors();
+    mouseinterval(0);
+    mousemask(ALL_MOUSE_EVENTS | REPORT_MOUSE_POSITION, NULL);
     init_pair(PAIR_NUM_YELLOW_FG, COLOR_YELLOW, -1);
     init_pair(PAIR_NUM_RED_FG, COLOR_RED, -1);
     init_pair(PAIR_NUM_BRIGHT_GRAY_FG, 251, -1);
