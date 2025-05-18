875824f826592e5e1640def003ac48726df0f4c9
xyzpw <76017734+xyzpw@users.noreply.github.com>
2025-05-17 23:49:50 -0400

    add menu panel

---
+++ include/structs/menu_panel.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <unordered_map>
+
+enum class PANEL_ITEM_ACTION {
+    QUIT,
+};
+
+enum class PANEL_ITEM_KEY {
+    QUIT,
+};
+
+struct MenuPanelItem {
+    PANEL_ITEM_ACTION action;
+
+    const char* text;
+    int row;
+
+    MenuPanelItem(PANEL_ITEM_ACTION a, int r, const char* txt)
+                : action(a), row(r), text(txt) {}
+};
+
+struct MenuPanel {
+    std::unordered_map<PANEL_ITEM_KEY, MenuPanelItem> items;
+
+    bool isOpen = false;
+    int colMin, colMax; // column range of menu panel appearance
+};
